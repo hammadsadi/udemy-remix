040cca2c18de5fe418bc20d4a2e986524f73260c
hammadsadi <hammad.sadi@yahoo.com>
2025-06-06 21:18:00 +0600

    feat: Implement Blog Title Update Functionality
    This commit introduces the ability to update the title of a blog post.
    
    - Implemented an `action` function to handle PATCH requests to update the blog title via the JSONPlaceholder API.
    - Added a form with a title input field and an update button.
    - Display a "Updating..." message while the form is submitting.
    - Utilized `useNavigation` hook to determine the submission state.

--- app/routes/blogs.$blogId.tsx
+++ app/routes/blogs.$blogId.tsx
@@ -1,5 +1,5 @@
-import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { ActionFunction, json, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, Link, useLoaderData, useNavigation } from "@remix-run/react";
 import React from "react";
 
 // Define Loader Function
@@ -10,20 +10,57 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   const blog = await res.json();
   return json({ blog });
 };
+
+// Action For Data Update
+export const action: ActionFunction = async ({ request, params }) => {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const res = await fetch(
+    ` https://jsonplaceholder.typicode.com/posts/${params?.blogId}`,
+    {
+      method: "PATCH",
+      body: JSON.stringify({ title }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+
+  const data = await res.json();
+  console.log(data);
+  return json({ data });
+};
+
 const BlogDetails = () => {
   const { blog } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const isSubmitting = !(navigation.state === "idle");
   return (
-    <div className="mt-7 text-center">
+    <div className="mt-7 text-center max-w-md mx-auto">
       <Link
         to="/"
         className="text-blue-500 py-2 px-4 bg-gray-800 mb-4 rounded-md"
       >
         Home
       </Link>
-      <div className="bg-white dark:bg-gray-900 p-8 shadow-md rounded-lg space-y-2 max-w-md mx-auto">
+      <div className="bg-white dark:bg-gray-900 p-8 shadow-md rounded-lg space-y-2 max-w-md mx-auto mb-3">
         <h1 className="text-3xl font-bold">{blog.title}</h1>
         <p>{blog.body}</p>
       </div>
+      <Form method="patch">
+        <div className="flex flex-col space-y-2">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            className="py-2 px-4 rounded-md"
+          />
+          <button className="bg-rose-500 py-2 px-4">
+            {" "}
+            {isSubmitting ? "Updating..." : "Update"}{" "}
+          </button>
+        </div>
+      </Form>
     </div>
   );
 };
